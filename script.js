6887fcc6090c8cb3d464faba6644a44dea55c604
Alex Burrows <burrows.alexj@gmail.com>
2024-05-19 18:59:25 -0400

    Fix javascript to work with buttons

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let humanScore = 0;
 let computerScore = 0;
 
-const rockButton = document.querySelector("#rockButton");
+const rockButton = document.querySelector(".rockButton");
 rockButton.addEventListener("click", function(){
     playRound("rock");
 })
 
-const paperButton = document.querySelector("#paperButton");
+const paperButton = document.querySelector(".paperButton");
 paperButton.addEventListener("click", function(){
     playRound("paper");
 })
 
-const scissorsButton = document.querySelector("#scissorsButton");
+const scissorsButton = document.querySelector(".scissorsButton");
 scissorsButton.addEventListener("click", function(){
     playRound("scissors");
 })
@@ -28,38 +28,38 @@ function getComputerChoice(){
             choice = "paper";
             break;
         case 2:
-            choice = "scisscors";
+            choice = "scissors";
             break;
         default:
-            choice = "choice is greater than 2";
+            choice = "Invalid choice greater than 2";
             break;
     }
     return choice;
 }
 
 function playRound(humanMove){
-    const winnerText = document.querySelector("#winnerText");
+    const winnerText = document.querySelector(".winnerText");
     humanScoreText = document.querySelector(".humanScore");
     computerScoreText = document.querySelector(".computerScore");
     let computerMove = getComputerChoice();
     
-    if (computerMove == humanMove){
+    if (computerMove === humanMove){
         winnerText.innerHTML = "Tie!";
     }
     else{
-        if ((computerMove == "rock" && humanMove == "paper")    ||
-            (computerMove == "paper" && humanMove == "scissors") || 
-            (computerMove == "scissors" && humanMove == "rock")) 
+        if ((computerMove == "rock" && humanMove == "scissors") ||
+            (computerMove == "paper" && humanMove == "rock")    || 
+            (computerMove == "scissors" && humanMove == "paper")) 
         {
             computerScore += 1;
             winnerText.innerHTML = "Computer Wins!";
-            computerScoreText.innerHTML = "Computer Score: " + computerScore;
+            computerScoreText.innerHTML = computerScore;
 
         }
         else{
             humanScore += 1;
             winnerText.innerHTML = "Human Wins!";
-            humanScoreText.innerHTML = "Human Score: " + humanScore;
+            humanScoreText.innerHTML = humanScore;
         }
     }
 }
